c6d31ce68da80a69fc3694d8fa0295dda0fc0baa
vardakisDev <vardakisdev@gmail.com>
2020-02-02 19:00:39 +0200

    fix

--- insta/insta.py
+++ insta/insta.py
@@ -13,12 +13,13 @@ data = []
 
 for post in islice(profile.get_posts(),20) :
     post_comments = post.get_comments()
-
     for comment in post_comments:
         data.append(comment.owner.username)
-print(data)
+
+# print(data)
+
 Counter = Counter(data) 
 most_occur = Counter.most_common(1)
-print('The three most active profiles in your comment section is:')
+print('The  most active profiles in your comment section is:')
 for i in range(len(most_occur)):
     print('1.',most_occur[i][0])
