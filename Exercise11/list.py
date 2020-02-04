3aa0463bb53a514459f56a254f3cf6cd8b72cbd1
vardakisDev <vardakisdev@gmail.com>
2020-02-04 18:57:29 +0200

    Added Exercise11

--- Exercise11/list.py
+++ Exercise11/list.py
@@ -1,8 +1,22 @@
 
 import random
+from ast import literal_eval
+# file = open("testfile.txt","w")
+# for i in range(0,100):
+#     line = str(random.sample(range(1,12),4))
+#     file.write(line)
+#     file.write('\n')
+# file.close()
 
-file = open("testfile.txt","w+")
-for i in range(0,100):
-    line = str(random.randint(1,14))+','
-    file.write(line)
-file.close()
+data = [numbers.rstrip('\n') for numbers in open("testfile.txt","r")]
+print(data)
+given = input('Give the the 6 exercises you want to look for , as 12,11,10,14:'  ).split(',')
+search = [int(numbers) for numbers in given]
+search = search[0:4]
+
+
+for numbers in data:
+    
+    if literal_eval(numbers)==search:
+        print('matched')
+        break
